689177a0be17927d335d2df05e85e78ae472f3b1
LeudjeuNjiatcha-Yanisael <leudjeuyannick33@gmail.com>
2026-02-28 23:19:40 +0100

    yo

--- commands/help.js
+++ commands/help.js
@@ -5,15 +5,15 @@ const path = require('path');
 async function helpCommand(sock, chatId, message) {
     const helpMessage = `
 ╔══════════════════════╗
-║ 🤖 *${settings.botName || 'MachineBot-RB3'}  SYSTEM*║
-╠══════════════════════╣
+║ 🤖 *${settings.botName || 'MachineBot-RB3'}  SYSTEM*   ║
+╠═════════════════════╣
 ║ ⚡ *Version*     : *${settings.version || '3.0.0'}*
 ║ 👨‍💻 *Developpeur* : *${settings.botOwner || 'Mr Robot'}*
 ║ 📳 *Contact*     : +237682441127
 ╚════════════════════╝
 
 ┌────────────────────────┐
-│👑*BIENVENUE DANS LE SYSTÈME*👑│
+👑*BIENVENUE DANS LE SYSTÈME*👑
 └────────────────────────┘
 _Tape une commande avec le préfixe (*)_
 
